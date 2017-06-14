ebb09fd7192cf52e765c7e3ce35130acb8bc8a88
Dmitry Tarantin <dimichspb@gmail.com>
2017-06-14 11:24:28 +0300

    Adding databasemanager

--- src/DatabaseServiceProvider.php
+++ src/DatabaseServiceProvider.php
@@ -15,5 +15,12 @@ class DatabaseServiceProvider extends ServiceProvider
         $this->app->singleton('db.factory', function ($app) {
             return new ConnectionFactory($app);
         });
+
+        // The database manager is used to resolve various connections, since multiple
+        // connections might be managed. It also implements the connection resolver
+        // interface which may be used by other components requiring connections.
+        $this->app->singleton('db', function ($app) {
+            return new DatabaseManager($app, $app['db.factory']);
+        });
     }
 }
